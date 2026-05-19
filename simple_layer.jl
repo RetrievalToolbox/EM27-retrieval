de4aab37a59fe918908d52167762167cde9617ae
PeterSomkuti <peter.somkuti@gmail.com>
2026-05-19 10:42:46 -0400

    Plot function fixes.

--- simple_layer.jl
+++ simple_layer.jl
@@ -336,7 +336,7 @@ function plot_timeseries(result_dict)
         scatter!(p, result_dict["Date"], result_dict["CO2"] * 1e6, markershape=:circle, label="GSFC", size=(1000, 300))
         plot!(p, df_ref.Date, df_ref.var" XCO2_STR", markershape=:star, label="PROFFAST XCO2_STR")
         scatter!(p, df_ref.Date, df_ref.var" XCO2", markershape=:square, label="PROFFAST XCO2")
-        #plot!(twinx(), result_dict["Date"], 1 ./ cosd.(result_dict["SZA"]), yaxis = "Airmass",
+        #plot!(p, twinx(), result_dict["Date"], 1 ./ cosd.(result_dict["SZA"]), yaxis = "Airmass",
         #    color=:black, label=nothing, linewidth=2)
         title!("XCO2")
         display(p)
@@ -346,7 +346,7 @@ function plot_timeseries(result_dict)
         p = plot()
         scatter!(p, result_dict["Date"], result_dict["H2O"] * 1e6, markershape=:circle, label="GSFC", size=(900, 300))
         scatter!(p, df_ref.Date, df_ref.var" XH2O", markershape=:square, label="PROFFAST")
-        #plot!(twinx(), result_dict["Date"], 1 ./ cosd.(result_dict["SZA"]), yaxis = "Airmass",
+        #plot!(p, twinx(), result_dict["Date"], 1 ./ cosd.(result_dict["SZA"]), yaxis = "Airmass",
         #    color=:black, label="Airmass", linewidth=2)
         title!("XH2O")
         display(p)
@@ -357,25 +357,29 @@ function plot_timeseries(result_dict)
         scatter!(p, result_dict["Date"], result_dict["CH4"] * 1e9, markershape=:circle, label="GSFC", size=(1000, 300))
         scatter!(p, df_ref.Date, df_ref.var" XCH4" * 1e3, markershape=:square, label="PROFFAST")
         scatter!(p, df_ref.Date, df_ref.var" XCH4_S5P" * 1e3, markershape=:square, label="PROFFAST")
-        #plot!(twinx(), result_dict["Date"], 1 ./ cosd.(result_dict["SZA"]), yaxis = "Airmass",
+        #plot!(p, twinx(), result_dict["Date"], 1 ./ cosd.(result_dict["SZA"]), yaxis = "Airmass",
         #    color=:black, label="Airmass", linewidth=2)
         title!("XCH4")
+        display(p)
     end
 
     if "CO" in keys(result_dict)
         p = plot()
         scatter!(p, result_dict["Date"], result_dict["CO"] * 1e9, markershape=:circle, label="GSFC", size=(1000, 300))
         scatter!(p, df_ref.Date, df_ref.var" XCO" * 1e3, markershape=:square, label="PROFFAST")
-        #plot!(twinx(), result_dict["Date"], 1 ./ cosd.(result_dict["SZA"]), yaxis = "Airmass",
+        #plot!(p, twinx(), result_dict["Date"], 1 ./ cosd.(result_dict["SZA"]), yaxis = "Airmass",
         #    color=:black, label="Airmass", linewidth=2)
         title!("XCO")
+        display(p)
     end
 
     if "O2" in keys(result_dict)
-        plot(p, result_dict["Date"], result_dict["O2"], markershape=:circle, label="GSFC", size=(1000, 300))
-        #plot!(twinx(), result_dict["Date"], 1 ./ cosd.(result_dict["SZA"]), yaxis = "Airmass",
+        p = plot()
+        plot!(p, result_dict["Date"], result_dict["O2"], markershape=:circle, label="GSFC", size=(1000, 300))
+        #plot!(p, twinx(), result_dict["Date"], 1 ./ cosd.(result_dict["SZA"]), yaxis = "Airmass",
         #    color=:green, label=nothing, linewidth=2)
         title!("XO2")
+        display(p)
     end
 
 end
